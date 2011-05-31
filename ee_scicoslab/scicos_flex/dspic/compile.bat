e48ea2e489be6b87607add6f1a1d3669a919533d
dario <dario@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2011-05-31 15:15:44 +0000

    ScicosLab: compile.bat usage description added (showed when parameters are missing).
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/scilab_codegen/trunk@1097 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- ee_scicoslab/scicos_flex/dspic/compile.bat
+++ ee_scicoslab/scicos_flex/dspic/compile.bat
@@ -5,8 +5,10 @@ rem * Are you here because the script can not find the bash shell?
 rem * Please change the following line!
 rem ***************************************************************
 set EE_BASH_PATH=C:\cygwin\bin\bash.exe
-
 if not exist %EE_BASH_PATH% goto error
+
+if [%1]==[] goto help
+if [%2]==[] goto help
 
 :ok
 set SHELLOPTS=igncr
@@ -18,5 +20,14 @@ echo *** ERROR:
 echo ***  The compile.bat script can not find the Cygwin BASH shell.
 echo ***  Please change the EE_BASH_PATH environment variable inside compile.bat
 pause
+goto end
+
+:help
+echo *** HELP:
+echo ***  Launch the compilation of the project.
+echo ***  Usage: compile.bat RT_DRUID_PATH PROJECT_TEMPLATE
+echo ***  example: compile.bat "C:/PROGRA~1/scicoslab/SCICOS~1.1/contrib/scicos_ee/RT-Druid" board_flex
+pause
+goto end
 
 :end
